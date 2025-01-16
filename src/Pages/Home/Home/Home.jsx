5b677acaec2cdb816472c30843ee78bcd82124d2
sadman sakib <sadmansakib1075@gmail.com>
2025-01-16 12:02:45 +0600

    added banner

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,9 +1,11 @@
 import About from "../About/About";
+import Banner from "../Banner/Banner";
 import Contact from "../Contact/Contact";
 
 const Home = () => {
     return (
         <div>
+            <Banner></Banner>
             <About></About>
             <Contact></Contact>
         </div>
